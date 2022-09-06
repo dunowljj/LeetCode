dfe0e9a12c78966d1b9944dc16745ef4214a34b0
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-09-07 08:51:54 +0900

    Time: 0 ms (100.00%), Space: 41.5 MB (66.18%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -15,6 +15,18 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        
+        root.left = right;
+        root.right = left;
+        
+        return root;
+        
+    }
+        /*
         if (root == null) return root;
         reverse(root);
         
@@ -34,4 +46,5 @@ class Solution {
         root.left = root.right;
         root.right = temp;
     }
+    */
 }
