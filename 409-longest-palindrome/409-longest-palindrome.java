3437cdac435acf0d897ed14d9e66e0adeddd2edf
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-09-12 08:16:57 +0900

    Time: 2 ms (93.33%), Space: 40.6 MB (93.88%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int answer = 0;
+        int[] counting = new int[52];
+        
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            
+            if ('a' <= curr && curr <= 'z') {
+                counting[curr - 'a']++;
+                continue;
+            }
+            
+            if ('A' <= curr && curr <= 'Z') {
+                counting[curr - 'A' + 26]++;
+                continue;
+            }
+        }
+        
+        boolean center = false;
+        for (int i = 0; i < 52; i++) {
+            int count = counting[i];
+            
+            answer += (count / 2) * 2;
+            
+            if (count % 2 == 1) {
+                center = true;
+            }
+        }
+        
+        if (center) {
+            answer++;
+        }
+        
+        return answer;
+    }
+}
+
+/*
+1 <= s.length <= 2000
+case sensitive
+s consists of lowercase and/or uppercase English letters only.
+
+method 1 
+counting array -> each time have to check is upper/lower
+
+method 2
+use hashSet or map
+*/
