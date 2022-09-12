27f8bec396055d9e39040c0373203deb59a3ae3f
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-09-12 09:18:29 +0900

    Time: 1 ms (13.92%), Space: 43 MB (51.15%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,26 +9,35 @@
  * }
  */
 class Solution {
-    ListNode tail;
+    ListNode tail; // remember lastNode
+    ListNode target; // location which needs to change link
     
     public ListNode reverseList(ListNode head) {
         if (head == null) {
             return head;
         }
-        reverse(head, head.next);
+        reverse(head);
         head.next = null;
         
         return tail;
     }
     
-    private void reverse(ListNode head, ListNode nextHead) {
+    private void reverse(ListNode head) {
+        
+        // find last node
         if (head.next == null) {
-            tail = head;
+            tail = head; 
+            target = head;
             return;
         }
         
-        reverse(head.next, nextHead.next);
-        nextHead.next = head;
+        reverse(head.next);
+        
+        // 타겟의 다음을 자신을 가리키던 노드로 변경
+        target.next = head;
+        
+        // 타겟을 타겟의 다음 노드(= 이전에 자신을 가리키던 노드)로 변경
+        target = target.next;
         
         return;
     }
