8274e0e3b3c78f0980c2dae91bbe00495f8c6f68
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-09-14 09:04:57 +0900

    Time: 0 ms (100.00%), Space: 41.3 MB (70.48%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -10,26 +10,39 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        return find(head);
-    }
-    
-    private ListNode find(ListNode head) {
-        int count = count(head);
-        count = count / 2; // count / 2 + 1 까지 도달해야함
+        ListNode fast = head;
+        ListNode slow = head;
         
-        while (count-- > 0) {
-            head = head.next;
-        }
-        return head;
-    }
-    
-    private int count(ListNode head) {
-        if (head == null) {
-            return 0;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
         
-        return count(head.next) + 1;
+        return slow;
     }
     
     
 }
+
+// public ListNode middleNode(ListNode head) {
+//         return find(head);
+//     }
+    
+//     private ListNode find(ListNode head) {
+//         int count = count(head);
+//         count = count / 2; // count / 2 + 1 까지 도달해야함
+        
+//         while (count-- > 0) {
+//             head = head.next;
+//         }
+//         return head;
+//     }
+    
+//     private int count(ListNode head) {
+//         if (head == null) {
+//             return 0;
+//         }
+        
+//         return count(head.next) + 1;
+//     }
+    
