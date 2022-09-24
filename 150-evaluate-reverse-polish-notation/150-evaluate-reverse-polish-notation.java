ddbe91e3bd21a7703c64ab126d8bd15e5b3856a6
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-09-25 08:43:03 +0900

    Time: 73 ms (5.05%), Space: 55.1 MB (5.01%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -5,8 +5,8 @@ class Calculator {
         this.symbol = input;
     }
     
-    static Calculator from(char symbol) {
-        return new Calculator(symbol);
+    static Calculator from(String token) {
+        return new Calculator(token.charAt(0));
     }
     
     static int operate(int operand1, int operand2) {
@@ -32,20 +32,20 @@ class Calculator {
 }
 
 class Solution {
-    Stack<Integer> numbers = new Stack();
         
     public int evalRPN(String[] tokens) {
+        Stack<Integer> numbers = new Stack();
+        
         for (int i = 0; i < tokens.length; i++) {
-            char curr = tokens[i].charAt(0);
-            
-            // -인 경우
             try {
-                numbers.push(Integer.parseInt(tokens[i]));    
+                numbers.push(Integer.parseInt(tokens[i]));  
+            
+            // 숫자로 변환 안되는 경우 -> 연산자    
             } catch (NumberFormatException e) {
                 int operand2 = numbers.pop();
                 int operand1 = numbers.pop();
                 
-                int result = Calculator.from(curr).operate(operand1, operand2);
+                int result = Calculator.from(tokens[i]).operate(operand1, operand2);
                 numbers.push(result);
             }
         }
