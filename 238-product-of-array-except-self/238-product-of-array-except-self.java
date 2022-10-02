f02107aa52b43e16029b999e5c4b01bd2cf82900
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-10-02 22:32:23 +0900

    Time: 8 ms (16.33%), Space: 58.3 MB (16.10%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -3,27 +3,22 @@ class Solution {
         
         int zeroCount = 0;
         int zeroIndex = 0;
-        
         int total = 1;
+        
+        int[] answer = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) { 
                 zeroCount++;
                 zeroIndex = i;
+                
+                if (zeroCount > 1) return answer;
             }
             
             else {
                 total *= nums[i];
             }
         }
-        
-        
-        int[] answer = new int[nums.length];
-        
-        if (zeroCount > 1) {
-            return answer;
-        }
-
-        
+                
         if (zeroCount == 1) {
             answer[zeroIndex] = total;
             return answer;
