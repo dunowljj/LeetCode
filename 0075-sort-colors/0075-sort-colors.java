6b2dcc381d3eae2644033127da3899e333668fc9
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-10-12 09:14:25 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (95.71%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,24 +1,35 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int[] lens = new int[3];
         
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) lens[0]++;
-            if (nums[i] == 1) lens[1]++;
-            if (nums[i] == 2) lens[2]++;
-        }
+        int front = 0;
+        int back = nums.length - 1;
         
-        lens[1] += lens[0];
-        lens[2] += lens[1];
-        
-        for (int i = 0; i < lens[2]; i++) {
-            if (i < lens[0]) {
-                nums[i] = 0;
-            } else if (i < lens[1]) {
-                nums[i] = 1;
-            } else {
-                nums[i] = 2;
+        for (int curr = 0; curr <= back; curr++) {
+            if (nums[curr] == 0) {
+                swap(nums, front, curr);
+                front++;
+            }
+            
+            else if (nums[curr] == 2) {
+                swap(nums, back, curr);
+                back--;
+                curr--;
             }
         }
     }
+    
+    private void swap(int[] nums, int idx1, int idx2) {
+        int temp = nums[idx1];
+        nums[idx1] = nums[idx2];
+        nums[idx2] = temp;
+    }
 }
+
+/*
+1 1 0 2 1 ; left 0 / right 4 / curr 0
+1 1 0 2 1 ; left 0 / right 4 / curr 1
+0 1 1 2 1 ; left 1 / right 4 / curr 2
+0 1 1 1 2 ; left 1 / right 3 / curr 3
+
+
+*/
