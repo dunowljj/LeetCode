7ce7500a1c6fa3f895c771633dd660a590f1041e
dunowljj <92416661+dunowljj@users.noreply.github.com>
2022-10-26 12:20:10 +0900

    Time: 5 ms (76.89%), Space: 81.3 MB (32.31%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -8,13 +8,19 @@ class Solution {
             max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
             
             if (height[left] < height[right]) {
-                left++;
+                int preLeft = height[left];
+                while (preLeft > height[++left]) {}
             }
             else if (height[left] > height[right]) {
-                right--;
+                int preRight = height[right];
+                while (preRight > height[--right]) {}
             } 
             else {
-                right--; left++; 
+                int preLeft = height[left];
+                int preRight = height[right];
+                
+                while (preLeft > height[++left]) {}
+                while (preRight > height[--right]) {}
             }
         }
         return max;
@@ -29,7 +35,7 @@ O(n^2) ??
 한 container 기준으로 바깥부터 다른 하나를 탐색하고, 더 긴그래프가 있을때만 검사하는건 어떨까?
 
 1 1 1 1 1 1 1 1 1 6 6 1 1 1 1 1 1 1 2
-
+3 2 2 2 1
 1 10 3 4 5 10 5
 
 discuss : 더 작은 포인터를 이동시키기. 같으면 둘 다 이동
