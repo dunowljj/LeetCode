f2e9a93ff7b9592306e18aa2151f84a1a83ce102
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-08-22 17:23:33 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,38 +10,26 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null && list2 == null) {
-            return null;
+        if (list1 == null || list2 == null) {
+            return list1 == null ? list2 : list1;
         }
 
-        ListNode mergedList = new ListNode();
-        mergeTwoLists(mergedList, list1, list2);
-        return mergedList;
-    }
+        if (list1.val > list2.val) {
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
+            
+            list1.next = mergeTwoLists(list1.next, list2.next);
+        }
 
-    public void mergeTwoLists(ListNode mergedList, ListNode list1, ListNode list2) {
-        if (list1 == null && list2 == null) {
-            return;
-        } else if (list1 == null) {
-            mergedList.val = list2.val;
-            mergedList.next = list2.next;
-            return;
-        } else if (list2 == null) {
-            mergedList.val = list1.val;
-            mergedList.next = list1.next;
-            return;
-        }
-        
-        if (list1.val <= list2.val) {    
-            mergedList.val = list1.val;
-            mergedList.next = new ListNode();
-            mergeTwoLists(mergedList.next, list1.next, list2);
-            return;
-        } else {
-            mergedList.val = list2.val;
-            mergedList.next = new ListNode();
-            mergeTwoLists(mergedList.next, list1, list2.next);
-            return;
-        }
+        return list1;
     }
 }
+/**
+함수 최초의 return은 결국에는 맨앞 요소중에 작은 요소를 가진 연결리스트의 헤드를 리턴한다.
+
+그 후에는 list1와 list2을 비교해서 list1에 더 작은 노드를 위치시키고,
+해당 더 작은 노드의 다음 노드에 연결될 값을 다음 재귀에서 찾는다.
+마찬가지로 다음 재귀에서 
+
+ */
