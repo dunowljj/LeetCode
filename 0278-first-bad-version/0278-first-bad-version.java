a84e2d3a51169b6983f65a439ea60d13b51cb333
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-09-29 11:26:28 +0900

    Time: 24 ms (98.08%), Space: 40.2 MB (67.59%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,20 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int lo = 1;
+        int hi = n;
+        
+        // F F F T T T T -> find first T
+        while (lo < hi) {
+
+            int mid = (hi - lo) / 2 + lo;
+
+            if (isBadVersion(mid)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+}
