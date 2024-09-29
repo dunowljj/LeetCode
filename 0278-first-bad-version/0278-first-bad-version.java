b1da59450819c5af86e9a0a41b657b404aa9b5b8
dunowljj <92416661+dunowljj@users.noreply.github.com>
2024-09-29 11:52:14 +0900

    Time: 25 ms (72.81%), Space: 40.7 MB (6.22%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -4,8 +4,8 @@ public class Solution extends VersionControl {
         int hi = n;
         
         // F F F T T T T -> find first T
-        while (lo + 1 <= hi) {
-
+        // lo + 1 <= hi 와 같이 설정시 overflow 유발 가능
+        while (lo < hi) {
             int mid = (hi - lo) / 2 + lo;
 
             if (isBadVersion(mid)) {
