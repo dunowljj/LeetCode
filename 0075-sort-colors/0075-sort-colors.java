1d9e0b6bba96e42cae8e01228802a4ed9540ad7a
dunowljj <92416661+dunowljj@users.noreply.github.com>
2026-01-16 14:10:01 +0900

    Time: 2 ms (18.37%), Space: 43.6 MB (47.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,35 +1,20 @@
 class Solution {
     public void sortColors(int[] nums) {
         
-        int front = 0;
-        int back = nums.length - 1;
-        
-        for (int curr = 0; curr <= back; curr++) {
-            if (nums[curr] == 0) {
-                swap(nums, front, curr);
-                front++;
-            }
-            
-            else if (nums[curr] == 2) {
-                swap(nums, back, curr);
-                back--;
-                curr--;
+        // bubble sort
+        int swapCount = 1;
+        while (swapCount != 0) {
+            swapCount = 0;
+
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    int temp = nums[i] ;
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                    swapCount++;
+                }
             }
         }
     }
-    
-    private void swap(int[] nums, int idx1, int idx2) {
-        int temp = nums[idx1];
-        nums[idx1] = nums[idx2];
-        nums[idx2] = temp;
-    }
 }
 
-/*
-1 1 0 2 1 ; left 0 / right 4 / curr 0
-1 1 0 2 1 ; left 0 / right 4 / curr 1
-0 1 1 2 1 ; left 1 / right 4 / curr 2
-0 1 1 1 2 ; left 1 / right 3 / curr 3
-
-
-*/
