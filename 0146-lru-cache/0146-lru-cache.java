9b3009ddf4144d2085f10f56517daee2532320fb
dunowljj <92416661+dunowljj@users.noreply.github.com>
2026-04-09 23:31:12 +0900

    Time: 44 ms (84.38%), Space: 131.1 MB (73.28%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,103 +1,119 @@
+import java.util.*;
+
 class LRUCache {
-    Map<Integer, Node> map = new HashMap();
-    Node head; //최신값의 앞
-    Node tail; //오래된 값의 뒤 -> 이 두개는 map에 더하지 않는다.
-    int capacity;
+
+    Map<Integer, Node> searchMap = new HashMap<>();
     
+    // pointers
+    Node recent;
+    Node old;
+    
+    int size = 0;
+    final int capacity;
+
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        
-        head = new Node();
-        tail = new Node();
-        head.next = tail;
-        tail.prev = head;
     }
     
-    // 최근에 사용한 여부를 나타내야함
     public int get(int key) {
-        if (map.containsKey(key)) {
-            Node node = map.get(key);
-
-            update(node);            
-            
-            return node.value;
+        if (!searchMap.containsKey(key)) {
+            return -1;
         }
+
+        Node find = searchMap.get(key);
         
-        return -1;
+        if (find != recent) {
+            moveToFirst(find);
+        } 
+        
+        return find.value;
     }
     
     public void put(int key, int value) {
-        
-        // 이미 키가 존재 : element 개수가 그대로이다. 
-        if (map.containsKey(key)) {
+        if (recent == null) {
+            size++;
             
-            Node node = map.get(key);
-            node.value = value;
+            Node newNode = new Node(key, value);
+            searchMap.put(key, newNode);
+
+            recent = newNode;
+            old = newNode;
+            return;
+        }
+
+        Node find = searchMap.get(key);
+        if (find == null) {
+            Node newNode = new Node(key, value);
+            searchMap.put(key, newNode);
+
+            // exceed
+            if (capacity == size) {
+                pasteFirst(newNode);
+
+                // remove last
+                searchMap.remove(old.key);
+                
+                Node before = old.before;
+                before.next = null;
+                old.before = null;
+                old = before;
+
+            } else {
+                pasteFirst(newNode);
+                size++;
+            }
             
-            update(node);
-        
-        // 키가 존재하지 않을 때 : element 개수가 추가되어 capacity를 신경써야한다.
+        // key is exist -> replace
         } else {
-            
-            // 추가하면 capacity 초과 -> 오래된 요소제거
-            if (map.size() == this.capacity) removeLRU();
-            
-            update(key, value);
+            find.value = value;
+            moveToFirst(find);
+        }
+    } 
+
+    private void moveToFirst(Node node) {
+        if (size == 1 || node == recent) return;
+
+        cut(node);
+        pasteFirst(node);
+    }
+    
+    private void cut(Node node) {
+        if (node == old) {
+            old = node.before;
+            node.before.next = null;
+            return;
+        }
+        
+        // when middle node
+        Node before = node.before;
+        Node next = node.next;
+        before.next = next;
+        next.before = before;
+        
+        node.before = null;
+        node.next = null;
+    }
+
+    private void pasteFirst(Node node) {
+        recent.before = node;
+        node.next = recent;
+        node.before = null;
+        recent = node;
+    }
+
+    static class Node {
+        int key;
+        int value;
+        Node before;
+        Node next;
+        
+        Node() {}
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
         }
     }
-    
-    // 이미 존재하는 노드 갱신
-    private void update(Node node) {
-        detach(node);
-        insert(node);
-    }
-    
-    private void detach(Node node) {
-        // 있던 자리에서 빼내고, 양쪽노드 이어주기 -> null일때 문제 발생할듯 -> head와 tail을 기본으로 양쪽에 두는 것으로 해결
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-    
-    // 최신값으로 만들기
-    private void insert(Node node) {
-        node.next = head.next;
-        head.next.prev = node;
-        
-        head.next = node;
-        node.prev = head;
-    }
-    
-    // 새로운 노드 갱신
-    private void update(int key, int value) {
-        Node node = new Node(key, value);
-        
-        insert(node);
-        map.put(key, node);
-    }
-    
-       
-    private void removeLRU() {
-        map.remove(tail.prev.key);
-                
-        // tail 앞의 요소에 대한 참조 제거
-        tail.prev = tail.prev.prev;
-        tail.prev.next = tail;
-    }
-}
-
-class Node {
-    int key;
-    int value;
-    Node prev;
-    Node next;
-    
-    Node() {}
-    
-    Node(int key, int value) {
-        this.key = key;
-        this.value = value;
-    }
-    
 }
 
 /**
@@ -105,12 +121,13 @@ class Node {
  * LRUCache obj = new LRUCache(capacity);
  * int param_1 = obj.get(key);
  * obj.put(key,value);
+
+init
+- LinkedList를 직접 구현
+
+put
+- 캐시에 값을 추가한다면, 맨 앞에 추가. 기존 key가 있는지 여부를 map으로 판단.
+- 기존 값을 대체해야 한다면 map으로 찾아가 변경 후, 맨 앞 순서로 이동시키기
+- 용량 초과 시, 가장 뒤에 오래된 값을 제거하고, 맨 앞에 새 값 추가
+
  */
-
-
-/*
-The functions get and put must each run in O(1) average time complexity.
-get, put -> O(1)
-수용량이 꽉찬 경우 가장 오래된 값을 없앤다. 
-
-*/
