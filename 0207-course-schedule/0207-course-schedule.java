6d7eccb625542576140a434b43425381eae85e3e
dunowljj <92416661+dunowljj@users.noreply.github.com>
2026-07-17 22:55:11 +0900

    Time: 116 ms (5.01%), Space: 47.8 MB (6.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,12 +1,18 @@
 class Solution {
 
     List<Integer>[] adj;
-    boolean[] visited;
+
+    /**
+        0 == not visited
+        1 == in dfs path
+        2 == complete
+     */
+    int[] state;
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        adj = new ArrayList[numCourses + 1];
+        adj = new ArrayList[numCourses];
 
-        for (int i = 0; i <= numCourses; i++) {
+        for (int i = 0; i < numCourses; i++) {
             adj[i] = new ArrayList<>();
         }
 
@@ -18,22 +24,30 @@ class Solution {
         }
 
         // 0~n-1
-        boolean canFinish = false;
-        for (int i = 0; i <= numCourses; i++) {
-            visited = new boolean[numCourses + 1];
-            canFinish |= hasCycle(i);
+        for (int i = 0; i < numCourses; i++) {
+            state = new int[numCourses];
+            if (state[i] == 0 && hasCycle(i)) return false;
         }
-        return !canFinish;
+
+        return true;
     }
 
-    private boolean hasCycle(int start) {
-        visited[start] = true;
+    private boolean hasCycle(int now) {
+        if (state[now] == 1) {
+            return true;
+        }
 
-        for (int next : adj[start]) {
-            if (visited[next]) return true;
+        if (state[now] == 2) {
+            return false;
+        }
+
+        state[now] = 1;
+        
+        for (int next : adj[now]) {
             if (hasCycle(next)) return true;
         }
 
+        state[now] = 2;
         return false;
     }
 }
