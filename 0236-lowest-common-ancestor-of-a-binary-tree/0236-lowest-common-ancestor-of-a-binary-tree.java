af76809ff2ae617f4805f56e1fe2bf5ccaef98de
dunowljj <92416661+dunowljj@users.noreply.github.com>
2026-07-22 23:00:06 +0900

    Time: 15 ms (74.35%), Space: 70 MB (28.12%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,26 +8,16 @@
  * }
  */
 class Solution {
-    
-    TreeNode first = null;
-    
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) return root;
-              
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        
+
         if (left != null && right != null) return root;
-    
-        // 이부분 고민해보기
-        if (left != null) return left;
-        else return right;
-    
+        else if (left != null) return left;
+        else if (right != null) return right;
+        else return null;
     }
 }
-
-/*
-p 아래 q || q 아래 p 있으면 큰 녀석이 LCA
-
-*/
