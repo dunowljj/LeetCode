379aad82003e633a4504b40291965f2bfcfc8ec1
dunowljj <92416661+dunowljj@users.noreply.github.com>
2026-07-25 19:13:56 +0900

    Time: 1 ms (86.74%), Space: 44 MB (75.61%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,24 +1,18 @@
 class Solution {
-  public List<List<Integer>> subsets(int[] nums) {
-    List<List<Integer>> output = new ArrayList();
-
-    int n = nums.length;
-    int nthBit = 1 << n;
-    
-    for (int i = 0; i < (int)Math.pow(2, n); i++) {
-        // 맨 앞의 0이 사라지는걸 방지하기 위해, 최대자릿수 + 1에 비트를 추가했다가 제거한다.
-        String bitmask = Integer.toBinaryString(i | nthBit).substring(1); 
-        
-        List<Integer> list = new ArrayList();
-        for (int j = 0 ; j < bitmask.length(); j++) {
-            if (bitmask.charAt(j) == '1') list.add(nums[j]);
-        }
-        
-        output.add(list);
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(result, new ArrayList<>(), nums, nums.length, 0);
+        return result;
     }
-      
 
-      
-    return output;
-  }
+    private void dfs(List<List<Integer>> result, List<Integer> set, int[] nums, int len, int idx) {
+        result.add(new ArrayList<>(set));
+        if (idx == len) return;
+
+        for (int i = idx; i < len; i++) {
+            set.add(nums[i]);
+            dfs(result, set, nums, len, i + 1);
+            set.remove(set.size() - 1);
+        }
+    }
 }
